9ae0f3c8e4391a17a15dc3829f1d4628cef8204f
shohagnawaz <snawazdigital@gmail.com>
2023-05-07 06:22:23 +0600

    key

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,26 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import Country from '../Country/Country';
+import './Countries.css';
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
     useEffect(() => {
         fetch("https://restcountries.com/v3.1/all")
-        .then(res => res.json())
-        .then(data => setCountries(data))
+            .then(res => res.json())
+            .then(data => setCountries(data))
     }, [])
     return (
         <div>
             <h1>I am coming form Countries</h1>
-            {
-                countries.map(country => <Country
-                    name={country.name.common}
-                    population={country.population}
-                    area={country.area}
-                ></Country>)
-            }
-             {
-                countries.map(country => console.log(country))
-            }
+            <div className='countries-container'>
+                {
+                    countries.map(country => <Country
+                        country={country}
+                        key = {country.cca3}
+                    ></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './Country.css';
 
 const Country = (props) => {
+    const {area, flags, population, name, region} = props.country;
     return (
         <div className='country'>
-            <h2>Name: {props.name}</h2>
-            <h4>Population: {props.population}</h4>
-            <p>Area: {props.area}</p>
+            <h2>Name: {name.common}</h2>
+            <img src={flags.png} alt="" />
+            <h4>Population: {population}</h4>
+            <p>Area: {area}</p>
+            <p>Region: {region}</p>
         </div>
     );
 };
